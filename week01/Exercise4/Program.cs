0f43b5cd5cccc2e8378de222204766597fa772f6
Maritherecua <mrtrsarroyo@yahoo.com>
2025-09-05 16:17:48 -0700

    completed requirements

--- week01/Exercise4/Program.cs
+++ week01/Exercise4/Program.cs
@@ -4,6 +4,51 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise4 Project.");
+        List<int> numbers = new List<int>();
+
+
+        // numbers.Add(i);
+        //Random random = new();
+        //int index = random.Next(numbers.Count)
+        // //int magicNumber = numbers[index];
+
+        int enteredNumber = -1;
+        while (enteredNumber != 0)
+        {
+            Console.Write("Please enter a number (0 to quit): ");
+            string enteredResponse = Console.ReadLine();
+            enteredNumber = int.Parse(enteredResponse);
+
+            if (enteredNumber != 0)
+            {
+                numbers.Add(enteredNumber);
+            }
+            Console.WriteLine("Numbers entered so far: " + string.Join(", ", numbers));
+        }
+        //Compute the sum and average of the numbers
+        int sum = 0;
+        foreach (int number in numbers)
+        {
+            sum += number;
+        }
+        Console.WriteLine("Sum: " + sum);
+        float average = (numbers.Count > 0) ? (float)sum / numbers.Count : 0;
+
+        Console.WriteLine("Average: " + average);
+        //Find the maximum number
+        int max = (numbers.Count > 0) ? numbers[0] : 0;
+        foreach (int number in numbers)
+        {
+            if (number > max)
+            {
+                max = number;
+            }
+
+        }
+
+        
     }
+    
+    
+    
 }
